e30c6ee86c993d77d32d9272c344f6e603267df9
Filipe Correia <f7lipe@Me.com>
2022-02-02 22:39:49 -0300

    feat: askcardNumber + verifyNumber
    Este commit adiciona duas funções que são carregadas ao abrir a página.  A função askCardNumber retorna um número recebido via promp, enquanto a função verifyNumber confere se o número informado obedece as regras definidas.

--- js/main.js
+++ js/main.js
@@ -3,4 +3,28 @@ function selectCard(card) {
     card.classList.toggle('virado')
 }
 
+// Retorna um número recebido via prompt
+function askCardNumber(){
+    return parseInt(prompt('Com quantas cartas você deseja jogar?'))
+}
 
+
+let evenNumber = false
+function verifyNumber(number) {
+    if (number >=2 && number <=14){
+        if (number % 2 === 0){
+            evenNumber = true
+            return true
+        }
+    }
+}
+
+let askedNumber = 0
+window.onload = function () {
+    while (evenNumber === false){
+        askedNumber = askCardNumber()
+        if (verifyNumber(askedNumber)){
+            //adicionar cartas 
+        }
+   }
+}
